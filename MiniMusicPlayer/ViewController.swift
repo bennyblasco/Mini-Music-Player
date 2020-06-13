9b1215a8bd60f46d6d94f933428b515595b90039
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-13 20:45:33 +0100

    Initial Commit

---
+++ MiniMusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniMusicPlayer
+//
+//  Created by Benjamin Inemugha on 13/06/2020.
+//  Copyright © 2020 Techelopers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
