020caa027614e038caa95306c665c2567f025ff9
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-13 21:34:07 +0100

    GenreButtonScreen View Controller added to MainStory board

---
+++ MiniMusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,30 @@
+//
+//  GenreButtonScreen.swift
+//  MiniMusicPlayer
+//
+//  Created by Benjamin Inemugha on 13/06/2020.
+//  Copyright © 2020 Techelopers. All rights reserved.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
