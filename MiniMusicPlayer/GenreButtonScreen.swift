cc60dac4455296ccafca20d2707238463168d2a2
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-13 22:43:28 +0100

    Implemented media player with UI

--- MiniMusicPlayer/GenreButtonScreen.swift
+++ MiniMusicPlayer/GenreButtonScreen.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class GenreButtonScreen: UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +19,34 @@ class GenreButtonScreen: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized{
+                self.playGenre(genre: sender.currentTitle!)
+            }
+        }
     }
-    */
-
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String){
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+        
+    }
+    
+    
 }
